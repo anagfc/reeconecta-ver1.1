03a5ed187b4ea0911cd17e4fbf9a9add9090feec
Ana Gabriela <httpsanagabriela@gmail.com>
2025-11-03 20:44:34 -0300

    Tela de login (Css), Formulário de cadastro de usuário e reestruturação da tabela user.

---
+++ reeconecta/Migrations/20251102143748_M05-UpdateTableUsuario.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace reeconecta.Migrations
+{
+    /// <inheritdoc />
+    public partial class M05UpdateTableUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TipoPerfil",
+                table: "Usuarios",
+                newName: "TipodePerfil");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailRepresentante",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomeFantasia",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RazaoSocial",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RepresentanteLegal",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoUsuario",
+                table: "Usuarios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WppTel1",
+                table: "Usuarios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WppTel2",
+                table: "Usuarios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailRepresentante",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "NomeFantasia",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "RazaoSocial",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "RepresentanteLegal",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "TipoUsuario",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "WppTel1",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "WppTel2",
+                table: "Usuarios");
+
+            migrationBuilder.RenameColumn(
+                name: "TipodePerfil",
+                table: "Usuarios",
+                newName: "TipoPerfil");
+        }
+    }
+}
---
+++ reeconecta/Migrations/20251103030721_M07-CorrecaoTableUsuarios.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace reeconecta.Migrations
+{
+    /// <inheritdoc />
+    public partial class M07CorrecaoTableUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RepresentanteLegal",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RazaoSocial",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeFantasia",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailRepresentante",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RepresentanteLegal",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RazaoSocial",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeFantasia",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailRepresentante",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
