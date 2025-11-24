b82fbc47718afca29abc94a5a93458a989a0a245
Geraldo Neres <geraldo.santos@gicbrasil.com>
2025-11-23 21:58:22 -0300

    Busca os produtos cadastrados

--- reeconecta/Controllers/HomeController.cs
+++ reeconecta/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using reeconecta.Models;
 
 namespace reeconecta.Controllers
@@ -7,15 +8,29 @@ namespace reeconecta.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var produtos = await _context.Produtos
+                .AsNoTracking()
+                .Where(p => p.StatusProduto == StatusProduto.Disponivel && 
+                           p.AnuncioAtivo &&
+                           p.Usuario != null && 
+                           p.Usuario.ContaAtiva)
+                .Include(p => p.Usuario)
+                .Include(p => p.ReservasProduto)
+                .OrderByDescending(p => p.CriacaoProduto)
+                .Take(6)
+                .ToListAsync();
+
+            return View(produtos);
         }
 
         public IActionResult Privacy()
